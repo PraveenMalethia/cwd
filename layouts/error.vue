5719d8034dc6bf8b4269ab101f654ec8e4a22c21
Parveen Malethia <parveen@coder.parveen>
2020-10-03 20:18:00 +0530

    Error Modified

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,20 +1,37 @@
 <template>
   <v-app dark>
+    <br><br>
     <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
+      <div class="d-flex justify-center">
+        <h2>
+          <glitch :text="pageNotFound" :background="false"></glitch>
+        </h2>
+      </div>
     </h1>
     <h1 v-else>
-      {{ otherError }}
+      <glitch :text="otherError" :background="false"></glitch>
     </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="d-flex justify-center">
+      <NuxtLink to="/">
+      <v-btn outlined x-large rounded depressed>
+      <glitch text="Home page" :background="false">
+      </glitch>
+      </v-btn>
+      </NuxtLink>
+    </div>
   </v-app>
 </template>
 
 <script>
+import Glitch from 'vue-glitch';
 export default {
   layout: 'empty',
+  components: { Glitch },
   props: {
     error: {
       type: Object,
@@ -40,5 +57,8 @@ export default {
 <style scoped>
 h1 {
   font-size: 20px;
+}
+a {
+  text-decoration:none;
 }
 </style>
