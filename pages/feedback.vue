4ea655028bf00abfa1d24fef3d112c53008666a2
Parveen Malethia <parveen@coder.parveen>
2020-10-15 21:52:30 +0530

    feedback form updated

--- pages/feedback.vue
+++ pages/feedback.vue
@@ -2,20 +2,65 @@
   <v-card class="mx-auto" max-width="500">
     <v-card-text>
       <p class="display-1 text--primary">Report Bug Or Feeback</p>
-      <div class="text--primary">
-        well meaning and kindly.
-        <br />"a benevolent smile"
-      </div>
+      <validation-observer ref="observer">
+    <form>
+      <validation-provider v-slot="{ errors }" name="Subject" rules="required|max:50">
+        <v-text-field v-model="subject" :error-messages="errors" label="Subject" required></v-text-field>
+      </validation-provider>
+      <validation-provider v-slot="{ errors }" name="Details" rules="required|max:2000">
+        <v-text-field v-model="details" :counter="2000" :error-messages="errors" label="Details" required></v-text-field>
+      </validation-provider>
+    </form>
+  </validation-observer>
     </v-card-text>
     <v-card-actions>
-      <v-btn text color="deep-purple accent-4">Submit</v-btn>
+      <v-btn class="ml-4 mb-4" @click="clear" text>clear</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn class="mr-4 mb-4" @click="submit" color="green darken-1"> submit</v-btn>
     </v-card-actions>
   </v-card>
 </template>
 
 <script>
+  import { required,max} from 'vee-validate/dist/rules'
+  import { extend, ValidationObserver, ValidationProvider, setInteractionMode } from 'vee-validate'
+
+  setInteractionMode('eager')
+
+  extend('required', {
+    ...required,
+    message: '{_field_} can not be empty',
+  })
+  extend('max', {
+  ...max,
+  message: '{_field_} may not be greater than {length} characters',
+  })
 export default {
+  components: {
+      ValidationProvider,
+      ValidationObserver,
+    },
   auth: false,
+  data: () => ({
+      subject: '',
+      details:'',
+      errors: null,
+    }),
+  methods: {
+      submit () {
+        this.$refs.observer.validate().then((response) => {
+          if (response == true){
+            this.$toast.success("Feedback submitted")
+            this.clear()
+          }
+        })
+      },
+      clear () {
+        this.subject = ''
+        this.details = ''
+        this.$refs.observer.reset()
+      },
+    },
   created(){
     document.title = 'CWD : Feedback'
   },
