8141a972cc1ec7f87214de1aa39d5aadab561f93
Parveen Malethia <parveen@coder.parveen>
2020-10-16 12:24:11 +0530

    profile card

--- pages/profile.vue
+++ pages/profile.vue
@@ -2,64 +2,113 @@
   <v-container>
     <v-layout row wrap>
       <v-flex class="pa-1" xs12 sm12 md6>
-        <v-card class="mx-auto" dark max-width="450">
-          <v-card-title>
-            <v-icon large left> mdi-account </v-icon>
-            <span class="title font-weight-light">Account</span>
-            <v-spacer></v-spacer>
-            <v-btn text @click="edit = !edit">Edit
-                  <v-icon right>mdi-account-edit</v-icon>
-                </v-btn>
-          </v-card-title>
-          <v-card-text class="headline">
-            <div v-if="!edit">
-              <div class="mt-2 ml-5">
-                <h5><v-icon left>mdi-account-circle</v-icon>Username : {{ $auth.user.username }}</h5>
-                <h5 class="mt-5"><v-icon left>mdi-account-box</v-icon>Name : {{ $auth.user.first_name }}{{ $auth.user.last_name }}</h5>
-                <h5 class="mt-5"><v-icon left>mdi-gmail</v-icon>Email : {{ $auth.user.email }}</h5>
-                <h5 class="mt-5"><v-icon left>mdi-phone</v-icon>Contact : {{ customer.phone_number }}</h5>
-              </div>
-            </div>
-            <div v-else>
-              <h3 class="ml-5 mt-2">Edit Profile</h3>
-              <div class="grey--text text-darken-1 mt-10 ml-5 mr-3">
-                <v-text-field
+        <v-card>
+        <v-card-title class="cyan darken-1">
+          <span class="headline white--text">Profile</span>
+          <v-spacer></v-spacer>
+          <v-btn v-if="edit" text @click="UpdateUser()">Update<v-icon right>mdi-update</v-icon></v-btn>
+          <v-btn dark icon @click="edit = !edit">
+            <v-icon>mdi-pencil</v-icon>
+          </v-btn>
+          <v-btn dark icon>
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </v-card-title>
+        <v-list v-if="!edit">
+          <v-list-item>
+            <v-list-item-action>
+              <v-icon>mdi-account-circle</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>Username : {{ $auth.user.username }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider inset></v-divider>
+          <v-list-item>
+            <v-list-item-action>
+              <v-icon>mdi-account-circle</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>{{ $auth.user.first_name }} {{$auth.user.last_name}}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider inset></v-divider>
+          <v-list-item>
+            <v-list-item-action>
+              <v-icon>mdi-phone</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>{{ customer.phone_number }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider inset></v-divider>
+          <v-list-item>
+            <v-list-item-action>
+              <v-icon>mdi-gmail</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>{{ $auth.user.email }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider inset></v-divider>
+          <v-list-item>
+            <v-list-item-action>
+              <v-icon>mdi-map-marker</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>Orlando, FL 79938</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+        <v-list v-else>
+          <v-list-item>
+            <v-list-item-action>
+              <v-icon>mdi-account-circle</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-text-field
                   v-model="user.username"
-                  prepend-icon="mdi-account-circle"
                   label="Username"
                   :placeholder="this.$auth.user.username"></v-text-field>
-                <v-text-field
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider inset></v-divider>
+          <v-list-item>
+            <v-list-item-action>
+              <v-icon>mdi-account-circle</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-text-field
                   v-model="user.first_name"
-                  prepend-icon="mdi-account"
                   label="First Name"
                   :placeholder="this.$auth.user.first_name"></v-text-field>
-                <v-text-field
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider inset></v-divider>
+          <v-list-item>
+            <v-list-item-action>
+              <v-icon>mdi-account-circle</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-text-field
                   v-model="user.last_name"
-                  prepend-icon="mdi-account"
                   label="Last Name"
                   :placeholder="this.$auth.user.last_name"></v-text-field>
-                <v-text-field
-                  v-model="UpdateCustomer.phone_number"
-                  prepend-icon="mdi-phone"
-                  label="Contact"
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider inset></v-divider>
+          <v-list-item>
+            <v-list-item-action>
+              <v-icon>mdi-phone</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-text-field v-model="UpdateCustomer.phone_number" label="Contact"
                   :placeholder="customer.phone_number"></v-text-field>
-                <v-btn color="deep-purple darken-1" @click="UpdateUser()">Update<v-icon right>mdi-update</v-icon></v-btn>
-              </div>
-            </div>
-          </v-card-text>
-          <v-card-actions>
-            <v-list-item class="grow">
-              <v-list-item-content>
-                <v-spacer></v-spacer>
-                <v-list-item-title v-if="customer.profile_pic">Change Profile Pic</v-list-item-title>
-                <v-list-item-title v-else>Upload Profile Pic</v-list-item-title>
-              </v-list-item-content>
-              <v-row align="center" justify="end">
-                <ProfilePicUpload />
-              </v-row>
-            </v-list-item>
-          </v-card-actions>
-        </v-card>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+        <v-img :src="'https://cwdstore.pythonanywhere.com' + customer.profile_pic" height="100%"></v-img>
+      </v-card>
       </v-flex>
       <v-flex class="pa-1" xs12 sm12 md6>
         <v-card class="mx-auto" max-width="500">
