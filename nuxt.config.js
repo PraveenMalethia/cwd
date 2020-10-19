fd18509a03b50226ce20548b69730e29225e7799
Parveen Malethia <parveen@coder.parveen>
2020-10-19 19:49:30 +0530

    pageTransition added

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   css: ['~/assets/main.css'],
-  transition: 'test',
+  pageTransition: 'test',
   middleware: 'auth',
   router: {
     middleware: ['auth']
