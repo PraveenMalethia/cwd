41b17d97c031a5f80f6b8193983399ce28ecb83c
Parveen Malethia <parveen@coder.parveen>
2020-10-20 17:21:24 +0530

    karan added in about

--- pages/about.vue
+++ pages/about.vue
@@ -9,7 +9,7 @@
     <v-flex class="text-center">
       <blockquote class="blockquote font-weight-bold">
         &#8220;NearBy Store Limited, NearBy Store Limited is an Indian e-commerce company based in Punjab, India.
-            It was founded by Parveen Malethia in 2020.
+            It was founded by Parveen Malethia and Karan Sarelia in 2020.
             The company initially focused on Grocery sales, after we are expanding into other product categories such as
             consumer electronics, fashion, home essentials , and lifestyle products..&#8221;
         <footer>
