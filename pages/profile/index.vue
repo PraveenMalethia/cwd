fbae5ce6f71d19821b42856651217be35a2854ce
Parveen Malethia <parveen@coder.parveen>
2020-10-25 17:17:13 +0530

    profile card fix

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -70,7 +70,7 @@
               </v-list-item-icon>
               <v-list-item-content>
                 <v-list-item-title>{{$auth.user.email}}</v-list-item-title>
-                <v-list-item-subtitle>Personal</v-list-item-subtitle>
+                <v-list-item-subtitle></v-list-item-subtitle>
               </v-list-item-content>
             </v-list-item>
           </v-list>
